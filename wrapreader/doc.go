3e216b988442f94ea13bb10e12507deec361f9ab
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2017-12-26 23:06:55 -0800

    Remove ReaderToReadCloser
    There's ioutil.NoCloser providing the same feature.

--- wrapreader/doc.go
+++ wrapreader/doc.go
@@ -3,7 +3,4 @@
 //
 // It's useful when dealing with io.Reader implementations that wraps another
 // io.ReadCloser, but will not close the underlying reader, such as GzipReader.
-//
-// It also provides a ReaderToReadCloser function to convert an io.Reader into
-// io.ReadCloser.
 package wrapreader
