b721ec1766be23f51da243fdbc01a69d4788a615
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2017-12-28 19:16:53 -0800

    remote: Add example

---
+++ remote/doc_test.go
@@ -0,0 +1,47 @@
+package remote_test
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/fishy/fsdb/bucket"
+	"github.com/fishy/fsdb/interface"
+	"github.com/fishy/fsdb/local"
+	"github.com/fishy/fsdb/remote"
+)
+
+func Example() {
+	localRoot, _ := ioutil.TempDir("", "fsdb_")
+	defer os.RemoveAll(localRoot)
+
+	var bucket bucket.Bucket
+	// TODO: open bucket from an implementation
+
+	ctx, cancel := context.WithCancel(context.Background())
+	db := remote.Open(
+		ctx,
+		local.Open(local.NewDefaultOptions(localRoot)),
+		bucket,
+		remote.NewDefaultOptions(),
+	)
+	defer cancel() // Stop the upload loop, not really necessary
+
+	key := fsdb.Key("key")
+
+	if err := db.Write(key, strings.NewReader("Hello, world!")); err != nil {
+		// TODO: handle error
+	}
+
+	reader, err := db.Read(key)
+	if err != nil {
+		// TODO: handle error
+	}
+	defer reader.Close()
+	// TODO: read from reader
+
+	if err := db.Delete(key); err != nil {
+		// TODO: handle error
+	}
+}
