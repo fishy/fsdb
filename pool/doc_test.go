bb117684d5678d28fbb8add9136bffd8e3593548
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2017-12-28 20:44:54 -0800

    pool: Add example

---
+++ pool/doc_test.go
@@ -0,0 +1,47 @@
+package pool_test
+
+import (
+	"fmt"
+
+	"github.com/fishy/fsdb/pool"
+)
+
+func Example() {
+	p := pool.NewPool(1)
+	gen := func(value string) pool.Generator {
+		return func() interface{} {
+			return value
+		}
+	}
+
+	fmt.Println(p.Size())
+	fmt.Println(p.Get(gen("generated data")))
+	fmt.Println(p.Size())
+	fmt.Println()
+	fmt.Println(p.Put("first data"))
+	fmt.Println(p.Size())
+	fmt.Println(p.Put("second data"))
+	fmt.Println(p.Size())
+	fmt.Println()
+	fmt.Println(p.Get(nil))
+	fmt.Println(p.Size())
+	fmt.Println()
+	fmt.Println(p.Get(gen("generated data")))
+	fmt.Println(p.Size())
+
+	// Output:
+	// 0
+	// generated data
+	// 0
+	//
+	// true
+	// 1
+	// false
+	// 1
+	//
+	// first data
+	// 0
+	//
+	// generated data
+	// 0
+}
