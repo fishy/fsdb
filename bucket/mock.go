9486f83257939fd4a261f3cab6e0b05a21096ae8
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2017-12-29 20:57:29 -0800

    bucket: Add docs to mock functions

--- bucket/mock.go
+++ bucket/mock.go
@@ -32,31 +32,35 @@ type Mock struct {
 	DeleteDelay MockOperationDelay
 }
 
-// MockBucket creates a new mock Bucket.
+// MockBucket creates a new mock Bucket using fsdb.
 func MockBucket(root string) *Mock {
 	return &Mock{
 		db: local.Open(local.NewDefaultOptions(root)),
 	}
 }
 
+// Read reads the file from fsdb.
 func (m *Mock) Read(ctx context.Context, name string) (io.ReadCloser, error) {
 	time.Sleep(m.ReadDelay.Before)
 	defer time.Sleep(m.ReadDelay.After)
 	return m.db.Read(ctx, fsdb.Key(name))
 }
 
+// Write writes the file to fsdb.
 func (m *Mock) Write(ctx context.Context, name string, data io.Reader) error {
 	time.Sleep(m.WriteDelay.Before)
 	defer time.Sleep(m.WriteDelay.After)
 	return m.db.Write(ctx, fsdb.Key(name), data)
 }
 
+// Delete deletes the file from fsdb.
 func (m *Mock) Delete(ctx context.Context, name string) error {
 	time.Sleep(m.DeleteDelay.Before)
 	defer time.Sleep(m.DeleteDelay.After)
 	return m.db.Delete(ctx, fsdb.Key(name))
 }
 
+// IsNotExist calls fsdb.IsNoSuchKeyError.
 func (m *Mock) IsNotExist(err error) bool {
 	return fsdb.IsNoSuchKeyError(err)
 }
