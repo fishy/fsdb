41b1a1aff4c415f8ad8d680801e4bbb43889e55a
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2017-12-29 19:33:03 -0800

    errbatch: Add real world example

--- errbatch/doc.go
+++ errbatch/doc.go
@@ -1,3 +1,29 @@
 // Package errbatch provides ErrBatch, which can be used to compile multiple
 // errors into a single error.
+//
+// An example of how to use it in your functions:
+//
+//     func runWorksParallel(works []func() error) error {
+//       errChan := make(chan error, len(works))
+//       var wg sync.WaitGroup
+//       wg.Add(len(works))
+//
+//       for _, work := range works {
+//         go func(work func() error) {
+//           errChan <- work()
+//         }(work)
+//       }
+//
+//       wg.Wait()
+//       close(errChan)
+//       errBatch := errbatch.NewErrBatch()
+//       for err := range errChan {
+//         // nil errors will be auto skipped
+//         errBatch.Add(err)
+//       }
+//       // If all works succeeded, Compile() returns nil.
+//       // If only one work failed, Compile() returns that error directly
+//       // instead of wrapping it inside ErrBatch.
+//       return errBatch.Compile()
+//     }
 package errbatch
