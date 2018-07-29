822817c58e181f1511b246c60dd85066d52aed7d
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2018-07-29 12:14:47 -0700

    bucket: Add Total to MockOperationDelay

--- bucket/mock.go
+++ bucket/mock.go
@@ -3,6 +3,7 @@ package bucket
 import (
 	"context"
 	"io"
+	"sync"
 	"time"
 
 	"github.com/fishy/fsdb"
@@ -12,8 +13,8 @@ import (
 // Make sure *Mock satisfies Bucket interface.
 var _ Bucket = (*Mock)(nil)
 
-// MockOperationDelay defines the delay before and after an operation.
-// It's useful to mimic network latency in tests.
+// MockOperationDelay defines the delays of an operation (function call).
+// It's useful to mimic network latency in local tests.
 type MockOperationDelay struct {
 	// Before is the delay between the function call and the actual operation.
 	Before time.Duration
@@ -21,6 +22,18 @@ type MockOperationDelay struct {
 	// After is the delay between the actual operation completes and the function
 	// returns.
 	After time.Duration
+
+	// Total is the minimal time the function call should take before returning.
+	// Here are some examples assuming the operation itself takes 1ms and we have
+	// a Total set to 50ms:
+	// - If both Before and After are set to zero, the function call will take
+	//   1ms to do the actual operation, then wait for ~49ms for the 50ms Total to
+	//   pass, so it returns after approximately 50ms;
+	// - If both Before and After are set to 30ms, the function call will first
+	//   sleep 30ms, then take 1ms to do the actual operation, then sleep 30ms
+	//   again. At this time the Total 50ms is already passed so the function
+	//   returns at approximately 61ms.
+	Total time.Duration
 }
 
 // Mock is a mock implementation of Bucket, backed by local FSDB.
@@ -41,6 +54,14 @@ func MockBucket(root string) *Mock {
 
 // Read reads the file from fsdb.
 func (m *Mock) Read(ctx context.Context, name string) (io.ReadCloser, error) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+	go func() {
+		defer wg.Done()
+		time.Sleep(m.ReadDelay.Total)
+	}()
+
 	time.Sleep(m.ReadDelay.Before)
 	defer time.Sleep(m.ReadDelay.After)
 	return m.db.Read(ctx, fsdb.Key(name))
@@ -48,6 +69,14 @@ func (m *Mock) Read(ctx context.Context, name string) (io.ReadCloser, error) {
 
 // Write writes the file to fsdb.
 func (m *Mock) Write(ctx context.Context, name string, data io.Reader) error {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+	go func() {
+		defer wg.Done()
+		time.Sleep(m.WriteDelay.Total)
+	}()
+
 	time.Sleep(m.WriteDelay.Before)
 	defer time.Sleep(m.WriteDelay.After)
 	return m.db.Write(ctx, fsdb.Key(name), data)
@@ -55,6 +84,14 @@ func (m *Mock) Write(ctx context.Context, name string, data io.Reader) error {
 
 // Delete deletes the file from fsdb.
 func (m *Mock) Delete(ctx context.Context, name string) error {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+	go func() {
+		defer wg.Done()
+		time.Sleep(m.DeleteDelay.Total)
+	}()
+
 	time.Sleep(m.DeleteDelay.Before)
 	defer time.Sleep(m.DeleteDelay.After)
 	return m.db.Delete(ctx, fsdb.Key(name))
--- bucket/mock_test.go
+++ bucket/mock_test.go
@@ -124,6 +124,80 @@ func TestMock(t *testing.T) {
 	}
 }
 
+func TestTotal(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	ctx := context.Background()
+
+	total := time.Millisecond * 50
+	shorter := time.Millisecond * 30
+
+	key := "foo"
+	data := "bar"
+
+	root, err := ioutil.TempDir("", "bucket_")
+	if err != nil {
+		t.Fatalf("failed to get tmp dir: %v", err)
+	}
+	defer os.RemoveAll(root)
+	mock := MockBucket(root)
+	db := mock.db
+	mock.WriteDelay = MockOperationDelay{
+		Before: shorter,
+		Total:  total,
+	}
+	mock.ReadDelay = MockOperationDelay{
+		Before: shorter,
+		After:  shorter,
+		Total:  total,
+	}
+
+	// Make sure it's empty
+	keys := scanKeys(t, db)
+	if len(keys) > 0 {
+		t.Fatalf("mock fsdb not empty: %v", keys)
+	}
+
+	// Write test
+	started := time.Now()
+	if err := mock.Write(ctx, key, strings.NewReader(data)); err != nil {
+		t.Errorf("write failed: %v", err)
+	}
+	elapsed := time.Now().Sub(started)
+	if elapsed <= total {
+		t.Errorf(
+			"write function should return after %v, actual time %v",
+			total,
+			elapsed,
+		)
+	}
+
+	// Delete test
+	started = time.Now()
+	closer, err := mock.Read(ctx, key)
+	if err != nil {
+		t.Fatalf("read failed: %v", err)
+	}
+	defer closer.Close()
+	elapsed = time.Now().Sub(started)
+	if elapsed <= total {
+		t.Errorf(
+			"delete function should return after %v, actual time %v",
+			total,
+			elapsed,
+		)
+	}
+	if elapsed <= shorter*2 {
+		t.Errorf(
+			"delete function should return after %v, actual time %v",
+			shorter*2,
+			elapsed,
+		)
+	}
+}
+
 func scanKeys(t *testing.T, db fsdb.Local) []fsdb.Key {
 	t.Helper()
 	ctx := context.Background()
